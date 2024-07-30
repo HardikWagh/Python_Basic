1030b21ce5c2c2a4ddbf6697e45ba5a2a8d6088d
HardikWagh <161699531+HardikWagh@users.noreply.github.com>
2024-07-30 13:44:46 +0530

    Create arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,17 @@
+a = 10
+b = 5
+# Addition
+sum_ = a + b
+print("Addition:", sum_)
+
+# Subtraction
+difference = a - b
+print("Subtraction:", difference)
+
+# Multiplication
+product = a * b
+print("Multiplication:", product)
+
+# Division
+quotient = a / b
+print("Division:", quotient)
