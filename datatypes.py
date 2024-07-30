73390a62e1b3af15cac6303a5f1f108e6e421613
HardikWagh <161699531+HardikWagh@users.noreply.github.com>
2024-07-30 13:45:48 +0530

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,15 @@
+# Integer
+num = 10
+print("Integer:", num)
+
+# Float
+pi = 3.14
+print("Float:", pi)
+
+# String
+name = "Alice"
+print("String:", name)
+
+# Boolean
+is_valid = True
+print("Boolean:", is_valid)
