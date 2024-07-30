41b36529e47ef95f615cfdc9563ee6ea827c6f77
HardikWagh <161699531+HardikWagh@users.noreply.github.com>
2024-07-30 13:49:14 +0530

    Create Loops.py

---
+++ Loops.py
@@ -0,0 +1,16 @@
+# If-Else
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
+# For Loop
+for i in range(5):
+    print("For loop iteration:", i)
+
+# While Loop
+count = 0
+while count < 5:
+    print("While loop iteration:", count)
+    count += 1
